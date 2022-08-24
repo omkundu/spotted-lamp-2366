8bdb15c5f2880cd9fe2cefd02e04404d67bcefd1
omkundu <omkundu156@gmail.com>
2022-08-24 12:00:10 +0530

    react-status-2

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <>
         <div>
          <Box bg='tomato' w='100%' p={4} color='white'>
-           This is the Box
+           This is the Box 2
          </Box>
         {/* style={{width:"100%",height:"100px",marginTop:"5rem",backgroundColor:"#545871"}} */}
         </div>
