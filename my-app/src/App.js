d81772355beb442452b6f6451b6ec992244d55ec
omkundu <omkundu156@gmail.com>
2022-08-24 11:56:55 +0530

    Setup-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Navbar from "./Components/Navbar"
+// import { Box } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+function Navbar() {
+  return (
+    <div>
+        {/* front-Page navbar start */}
+        <>
+        <div>
+         <Box bg='tomato' w='100%' p={4} color='white'>
+           This is the Box
+         </Box>
+        {/* style={{width:"100%",height:"100px",marginTop:"5rem",backgroundColor:"#545871"}} */}
+        </div>
+
+        </>
+      
+    </div>
+  )
+}
+
+export default Navbar
